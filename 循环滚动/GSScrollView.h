c76769bed84e8fec6d5ed4b965c27526b23d7765
@LiYanBing <@gsliyanbing@163.com>
2015-10-31 14:25:22 +0800

    第一次提交

---
+++ 循环滚动/GSScrollView.h
@@ -0,0 +1,13 @@
+//
+//  GSScrollView.h
+//  循环滚动
+//
+//  Created by 李彦兵 on 15/10/31.
+//  Copyright © 2015年 李彦兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GSScrollView : UIView
+
+@end
---
+++ 循环滚动/GSScrollView.m
@@ -0,0 +1,21 @@
+//
+//  GSScrollView.m
+//  循环滚动
+//
+//  Created by 李彦兵 on 15/10/31.
+//  Copyright © 2015年 李彦兵. All rights reserved.
+//
+
+#import "GSScrollView.h"
+
+@implementation GSScrollView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
