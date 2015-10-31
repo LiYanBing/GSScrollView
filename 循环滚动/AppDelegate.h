17bb7237247390de5eb20c19eae9d30ade69b517
@LiYanBing <@gsliyanbing@163.com>
2015-10-31 12:46:16 +0800

    Initial Commit

---
+++ 循环滚动/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  循环滚动
+//
+//  Created by 李彦兵 on 15/10/31.
+//  Copyright © 2015年 李彦兵. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
